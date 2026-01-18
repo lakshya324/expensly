07d2e1d3df5988689cd52c6bf128204f4499e726
Lakshya Sharma <lakshya.off31@gmail.com>
2026-01-18 21:42:03 +0530

    update session keys

--- frontend/js/config/env.config.js
+++ frontend/js/config/env.config.js
@@ -1,9 +1,13 @@
 export const API_BASE = "http://localhost:3000/api";
-export const USER_SESSION_KEY = "expensly_user_session";
 
 export const USER_CURRENCY_KEY_PREFIX = "expensly_currency_";
 
 export const INDEXED_DB_CONFIG ={
     name: "ExpenslyDB",
     version: 1,
+}
+
+export const SESSION_KEY = {
+    user: "expensly_user_session",
+    draft: "expensly_draft_expense",
 }
