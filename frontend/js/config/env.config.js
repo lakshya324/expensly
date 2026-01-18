0cb0e57cc38359e98f2f5daf735a2dcca3befa0b
Lakshya Sharma <lakshya.off31@gmail.com>
2026-01-18 18:48:21 +0530

    creating common config

---
+++ frontend/js/config/env.config.js
@@ -0,0 +1,9 @@
+export const API_BASE = "http://localhost:3000/api";
+export const USER_SESSION_KEY = "expensly_user_session";
+
+export const USER_CURRENCY_KEY_PREFIX = "expensly_currency_";
+
+export const INDEXED_DB_CONFIG ={
+    name: "ExpenslyDB",
+    version: 1,
+}
