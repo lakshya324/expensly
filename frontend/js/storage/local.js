ab88a76f1fa6b29c07b5e917ab0c1c0b7d1bbadc
Lakshya Sharma <lakshya.off31@gmail.com>
2026-01-18 21:52:15 +0530

    adding local storage

---
+++ frontend/js/storage/local.js
@@ -0,0 +1,16 @@
+import { LOCAL_KEYS } from "../config/env.config";
+
+export class UserPreferenceLocal {
+  static set(preference) {
+    localStorage.setItem(LOCAL_KEYS.userPreference, JSON.stringify(preference));
+  }
+
+  static get() {
+    const info = localStorage.getItem(LOCAL_KEYS.userPreference);
+    return info ? JSON.parse(info) : null;
+  }
+
+  static clear() {
+    localStorage.removeItem(LOCAL_KEYS.userPreference);
+  }
+}
