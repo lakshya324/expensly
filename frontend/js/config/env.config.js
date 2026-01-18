f2275205949ecbc1a8b1ba15cc9b55041aca153f
Lakshya Sharma <lakshya.off31@gmail.com>
2026-01-18 22:04:23 +0530

    removing unused key

--- frontend/js/config/env.config.js
+++ frontend/js/config/env.config.js
@@ -1,7 +1,5 @@
 export const API_BASE = "http://localhost:3000/api";
 
-export const USER_CURRENCY_KEY_PREFIX = "expensly_currency_";
-
 export const INDEXED_DB_CONFIG ={
     name: "ExpenslyDB",
     version: 1,
