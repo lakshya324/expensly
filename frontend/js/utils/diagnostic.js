e9f11985965b094795c2f3656d8a5ce28ff7788a
Lakshya Sharma <lakshya.off31@gmail.com>
2026-01-19 21:20:28 +0530

    adding dummy diagnostic logic

---
+++ frontend/js/utils/diagnostic.js
@@ -0,0 +1,6 @@
+export function setupDiagnosticButton() {
+  document.getElementById("btn-diagnostic").addEventListener("click", () => {
+    // Cando: add detailed diagnostics here
+    alert("wow what a Nice code... No bugs found! ;)");
+  });
+}
