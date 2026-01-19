83a93d093e7324770b0a831bef1e5117f9d55971
Lakshya Sharma <lakshya.off31@gmail.com>
2026-01-19 16:27:51 +0530

    add more form functions

--- frontend/js/ui/form.js
+++ frontend/js/ui/form.js
@@ -7,3 +7,19 @@ export function getExpenseFormData() {
     tags: document.getElementById("expense-tags").value,
   };
 }
+
+export function clearExpenseForm() {
+  document.getElementById("expense-title").value = "";
+  document.getElementById("expense-amount").value = "";
+  // document.getElementById("expense-dept").value = "";
+  document.getElementById("expense-desc").value = "";
+  document.getElementById("expense-tags").value = "";
+}
+
+export function populateExpenseForm(data) {
+  document.getElementById("expense-title").value = data.title || "";
+  document.getElementById("expense-amount").value = data.amount || "";
+  // document.getElementById("expense-dept").value = data.department || "";
+  document.getElementById("expense-desc").value = data.description || "";
+  document.getElementById("expense-tags").value = data.tags || "";
+}
