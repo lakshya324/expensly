8a047b7b620fcd3c630d6fd14040437b68c3d42c
Lakshya Sharma <lakshya.off31@gmail.com>
2026-01-19 18:28:50 +0530

    adding budget in local

--- frontend/js/storage/local.js
+++ frontend/js/storage/local.js
@@ -1,4 +1,5 @@
 import { LOCAL_KEYS } from "../config/env.config.js";
+import { CURRENCY } from "../utils/currency.js";
 
 export class UserPreferenceLocal {
   static set(preference) {
@@ -12,5 +13,20 @@ export class UserPreferenceLocal {
 
   static clear() {
     localStorage.removeItem(LOCAL_KEYS.userPreference);
+  }
+}
+
+export class BudgetLocal {
+  static set(budgetData) {
+    localStorage.setItem(LOCAL_KEYS.budgetData, JSON.stringify(budgetData));
+  }
+
+  static get() {
+    const data = localStorage.getItem(LOCAL_KEYS.budgetData);
+    return data ? JSON.parse(data) : null;
+  }
+
+  static clear() {
+    localStorage.removeItem(LOCAL_KEYS.budgetData);
   }
 }
