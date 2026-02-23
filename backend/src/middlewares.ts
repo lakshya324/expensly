d99460220b58d5be871c072e287f5c3784490d2b
Lakshya Sharma <lakshya.off31@gmail.com>
2026-02-23 16:55:18 +0530

    feat: enable trust proxy setting in middleware for improved client IP fetching

--- backend/src/middlewares.ts
+++ backend/src/middlewares.ts
@@ -10,6 +10,9 @@ export default function middlewares(app: express.Application) {
   app.use(helmet());
   app.use(corsMiddleware);
 
+  //! Fetching Client IP
+  app.set("trust proxy", true);
+
   //! Body Parsing
   app.use(express.json({ limit: "1mb" }));
   app.use(express.urlencoded({ extended: true }));
